2ff16794942bbb1770113b698701aaa3aa09ae9f
AndriyKlymash <andriyklymash2020@gmail.com>
2021-06-02 00:31:25 +0300

    practice_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {UsersComponent} from './components/users/users.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {UserComponent} from './components/user/user.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { PostComponent } from './components/post/post.component';
+
+const routes: Routes = [
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id', component: UserDetailsComponent}
+    ]
+  },
+
+  {path: 'posts', component: PostsComponent, children:[
+      {path: ':id', component: PostDetailsComponent}
+    ]},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    PostsComponent,
+    UserComponent,
+    UserDetailsComponent,
+    PostDetailsComponent,
+    PostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
