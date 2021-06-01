81632e788ffddadb8e1f24ba2f20db18b1273f62
AndriyKlymash <andriyklymash2020@gmail.com>
2021-06-01 14:03:51 +0300

    practice_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../models/Post";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private httpClient:HttpClient) {
+  }
+
+  getPosts():Observable<Post[]>{
+    return this.httpClient.get<Post[]>(this.url);
+  }
+
+  getPostsByUserId(id:number):Observable<Post[]>{
+    const headers = new HttpHeaders()
+    const params = new HttpParams().append('userId', id);
+    return this.httpClient.get<Post[]>(this.url,{headers, params});
+}
+}
